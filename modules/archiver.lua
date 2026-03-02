8e92503f5ddf632fed28cf4c1b45f97f31d5b6ff
Maximus7474 <the.maximus.7474@gmail.com>
2026-03-02 16:48:18 +0100

    feat(archiver): commands to clear, load and dump archive data

--- modules/archiver.lua
+++ modules/archiver.lua
@@ -108,6 +108,81 @@ function Archiver:SaveArchiveData()
 	SetResourceKvp('archivedata', json.encode(self.data))
 end
 
+---clear archive data
+function Archiver:ClearArchiveData()
+	self.data = {}
+	self:SaveArchiveData()
+end
+
+---set archive data from an external source
+function Archiver:SetArchiveData(data)
+	self.data = data
+	self:SaveArchiveData()
+end
+
+---clear archive data
+function Archiver:DumpArchiveData()
+	return json.encode(
+		self.data,
+		{ indent=true, sort_keys=true }
+	)
+end
+
+-- quick command to clear the kvp where all backup data is stored
+RegisterCommand('clear_kvp', function (source, args)
+	if source ~= 0 then return end
+
+	local confirmation = tostring(args[1] or 'no'):lower():gsub("%s+", "")
+    if confirmation ~= 'yes' then
+        print('[ARCHIVER] ^3WARNING:^7 This will wipe all backup metadata. Files will remain, but the script will "forget" their ages.')
+        print('[ARCHIVER] ^3USAGE:^7 clear_kvp yes')
+        return
+    end
+
+	Archiver:ClearArchiveData()
+	print('^2[ARCHIVER]^7 KVP storage and local cache have been cleared.')
+end, true)
+
+-- quick command to dump the kvp into a json file
+RegisterCommand('dump_kvp', function (source, args)
+	if source ~= 0 then return end
+
+	local resourceName = GetCurrentResourceName()
+	local payload = Archiver:DumpArchiveData()
+	local success = SaveResourceFile(resourceName, 'archiveData.json', payload, -1)
+
+    if success then
+        print(string.format('^2[ARCHIVER]^7 Data exported to: ^3%s/archiveData.json^7', resourceName))
+    else
+        print('^1[ARCHIVER] ERROR:^7 Failed to write file. Check folder permissions.')
+    end
+end, true)
+
+-- loads archive data metadata from the archiveData.json file
+RegisterCommand('load_kvp', function(source, args)
+    if source ~= 0 then return end
+
+    local resourceName = GetCurrentResourceName()
+    local fileContent = LoadResourceFile(resourceName, 'archiveData.json')
+
+    if not fileContent then
+        error('[ARCHIVER] Could not find "archiveData.json" in the resource folder.')
+    end
+
+    local data = json.decode(fileContent)
+
+    if type(data) ~= 'table' then
+        error('[ARCHIVER] "archiveData.json" contains invalid data (not a JSON object).')
+    end
+
+    Archiver:SetArchiveData(data)
+
+    local count = 0
+    for _ in pairs(data) do count = count + 1 end
+
+    print(string.format('[ARCHIVER] Successfully imported "^3%s^7" metadata entries from JSON.', count))
+end, true)
+
 CreateThread(function ()
 	Archiver:Initialize()
 end)
