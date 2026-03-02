9e5633226f66d12c2a375ee868c8bc26ef7193e5
Maximus7474 <the.maximus.7474@gmail.com>
2026-03-02 13:28:46 +0100

    feat(init): initialize ox lib cron job

--- modules/init.lua
+++ modules/init.lua
@@ -1,7 +1,7 @@
 RegisterCommand('backup_database', function (source, args)
 	if source ~= 0 then return end
 
-	local fileName = args[1] or string.format('backup-%s', os.date("%Y%m%d-%H%M%S"))
+	local fileName = args[1] or BackupFileName()
 	local backupPath = string.format('backups/%s.sql', fileName)
 
 	print(string.format('Saving database backup to: "^3%s^7"', backupPath))
@@ -9,3 +9,28 @@ RegisterCommand('backup_database', function (source, args)
 	local payload = DB:CreateFullBackup()
 	SaveResourceFile(GetCurrentResourceName(), backupPath, payload, -1)
 end, true)
+
+if Config.Cron.Enabled then
+	LoadOxLib()
+
+	local cronExpression
+	if Config.Cron.ExpressionOverride then
+		cronExpression = Config.Cron.ExpressionOverride
+	else
+		cronExpression = string.format(
+			'0 %d %d * *',
+			Config.Cron.Hour,
+			Config.Cron.DayFrequency
+		)
+	end
+
+	lib.cron.new(cronExpression, function (task, date)
+		local fileName = BackupFileName()
+		local backupPath = string.format('backups/%s.sql', fileName)
+
+		print(string.format('[CRON] Saving database backup "^3%s^7"', fileName))
+
+		local payload = DB:CreateFullBackup()
+		SaveResourceFile(GetCurrentResourceName(), backupPath, payload, -1)
+	end)
+end
